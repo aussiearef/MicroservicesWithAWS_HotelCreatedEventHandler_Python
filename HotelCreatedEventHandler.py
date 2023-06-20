203e5e202349275809ffe216a99d26d01ee9368c
aussiearef <arefkr@gmail.com>
2023-06-20 21:01:57 +1000

    Added HotelCreatedEventHandler.py

--- HotelCreatedEventHandler.py
+++ HotelCreatedEventHandler.py
@@ -1,68 +1,39 @@
-import os
 import json
-from datetime import datetime
-from typing import List
+import os
+import boto3
+from boto3 import dynamodb
+from elasticsearch import Elasticsearch # pip install elasticsearch
 
-from aws_lambda_powertools.logging import Logger
-from aws_lambda_powertools.utilities.typing import LambdaContext
-from aws_lambda_powertools.event_processor.handler import CoroutineHandler
-from aws_lambda_powertools.event_processor.models import SNSEvent
-from elasticsearch import Elasticsearch, helpers
-from boto3 import client
-from boto3.dynamodb.conditions import Key
+def handler(event, context):
 
-logger = Logger(service="HotelCreatedEventHandler")
-db_client = client("dynamodb")
-table = db_client.Table("hotel-created-event-ids")
+    message_id = event['Records'][0]['Sns']["MessageId"]
+    sns_message = event['Records'][0]['Sns']['Message']
+    sns_message_json = json.loads(sns_message)
 
-host = os.getenv("host")
-user_name = os.getenv("userName")
-password = os.getenv("password")
-index_name = os.getenv("indexName")
+    tableName = os.environ.get("hotelCreatedEventIdsTable")
 
-es = Elasticsearch(host, http_auth=(user_name, password), scheme="https", port=443)
+    dynamodb_client = boto3.client('dynamodb')
+    response = dynamodb_client.get_item(
+        TableName = tableName,
+        Key={
+            "eventId":{"S": message_id}
+        }
+    )
 
-class Hotel:
-    def __init__(self, name: str, city_name: str, price: int, rating: int,
-                 user_id: str, id: str, creation_date_time: str, file_name: str):
-        self.name = name
-        self.city_name = city_name
-        self.price = price
-        self.rating = rating
-        self.user_id = user_id
-        self.id = id
-        self.creation_date_time = creation_date_time
-        self.file_name = file_name
-
-
-class HotelCreatedEventHandler(CoroutineHandler):
-    async def process_event(self, sns_event: SNSEvent, context: LambdaContext) -> None:
-        logger.info("Lambda was invoked.")
-        logger.info(f"Found {len(sns_event.records)} records in SNS Event")
-
-        for event_record in sns_event.records:
-            event_id = event_record.sns.message_id
-            found_item = table.query(KeyConditionExpression=Key("eventId").eq(event_id))
-            if not found_item["Items"]:
-                table.put_item(Item={"eventId": event_id})
-
-            hotel = json.loads(event_record.sns.message, object_hook=lambda d: Hotel(**d))
-
-            index_action = {
-                "_index": index_name,
-                "_id": hotel.id,
-                "_source": {
-                    "name": hotel.name,
-                    "cityName": hotel.city_name,
-                    "price": hotel.price,
-                    "rating": hotel.rating,
-                    "userId": hotel.user_id,
-                    "creationDateTime": datetime.fromisoformat(hotel.creation_date_time).isoformat(),
-                    "fileName": hotel.file_name
+    if 'Items' not in response:
+        dynamodb_client.put_item(
+            TableName=tableName,
+            Item={
+                "eventId":{
+                    "S": message_id
                 }
             }
+        )
 
-            response = helpers.bulk(es, [index_action])
-            if response[0].get("index", {}).get("error"):
-                logger.error(f"Server Error: {response[0]['index']['error']['reason']}")
+    host = os.getenv("host")
+    user_name = os.getenv("userName")
+    password =os.getenv("password")
+    index_name = os.getenv("indexName")
 
+    es = Elasticsearch(host, http_auth=(user_name, password), scheme="https", port=443)
+    es.index(index=index_name, doc_type="doc", id=sns_message_json["Id"], body= sns_message_json)
