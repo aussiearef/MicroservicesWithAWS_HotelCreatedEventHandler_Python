c44a68b3ae6ed3868a54ac73a410475a756c32b4
Aref Karimi <arefkr@gmail.com>
2023-06-21 16:48:14 +1000

    Update HotelCreatedEventHandler.py

--- HotelCreatedEventHandler.py
+++ HotelCreatedEventHandler.py
@@ -7,7 +7,7 @@ from elasticsearch import Elasticsearch # pip install elasticsearch
 def handler(event, context):
 
     message_id = event['Records'][0]['Sns']["MessageId"]
-    sns_message = event['Records'][0]['Sns']['Message']
+    sns_message = event['Records'][0]['Sns']["Message"]
     sns_message_json = json.loads(sns_message)
 
     tableName = os.environ.get("hotelCreatedEventIdsTable")
@@ -29,11 +29,11 @@ def handler(event, context):
                 }
             }
         )
+        
+        host = os.getenv("host")
+        user_name = os.getenv("userName")
+        password =os.getenv("password")
+        index_name = os.getenv("indexName")
 
-    host = os.getenv("host")
-    user_name = os.getenv("userName")
-    password =os.getenv("password")
-    index_name = os.getenv("indexName")
-
-    es = Elasticsearch(host, http_auth=(user_name, password), scheme="https", port=443)
-    es.index(index=index_name, doc_type="doc", id=sns_message_json["Id"], body= sns_message_json)
+        es = Elasticsearch(host, http_auth=(user_name, password)) # include scheme and port in host url i.e., https://localhost:9200
+        es.index(index=f"{index_name}", id=sns_message_json["Id"], body=sns_message_json) #doc_type is now deprecated
